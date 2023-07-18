aba79cf51e4b578f4a7d51e7eb7ea47ae1a5c15c
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-18 18:54:14 +0300

    Bacground color

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { Aside } from './components/aside/Aside';
 import { Header } from './components/header/Header';
 import { Main } from './components/main/Main';
 import './App.css';
+import Helmet from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Aside />
+      <Helmet bodyAttributes={{style: 'background-color : #f5f5fb'}}/>
     </div>
   );
 }
+
+
 
 export default App;
