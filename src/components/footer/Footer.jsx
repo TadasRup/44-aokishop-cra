74dc2b2bab3d2c1a43f8fa986b56e00d43f45aa5
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-20 19:56:21 +0300

    text grid

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,58 @@
-import style from './Footer.module.css';
 import React from "react";
 import GridLayout from "./GridLayout";
+import Footer1 from "../../img/footer1.png";
+import Footer2 from "../../img/footer2.png";
+// Import other item photos and define your items array here.
 
 const Footer = () => {
+    const items = [
+      {
+        //photo: Footer1,
+        section1: "Small telecast live",
+        section2: "Live sales in WeChat",
+      },
+      {
+       // photo: Footer2,
+        section1: "Mini Program Store",
+        section2: "WeChat Mini Program Store",
+      },
+      {
+        // photo: Footer3,
+         section1: "Distribution marker",
+         section2: "One-click promotion",
+       },
+       {
+        // photo: Footer4,
+         section1: "Coupon",
+         section2: "Store marketing essentials",
+       },
+       {
+        // photo: Footer5,
+         section1: "Store construction",
+         section2: "WCan be customized",
+       },
+       {
+        // photo: Footer6,
+         section1: "Promoting Ads",
+         section2: "WeChat billion-level exposure",
+       },
+       {
+        // photo: Footer7,
+         section1: "Promotion rebate",
+         section2: "Help you sell goods",
+       },
+       {
+        // photo: Footer8,
+         section1: "Asset Management",
+         section2: "Income and withdrawal",
+       },
+    ];
+  
     return (
-      <div className={style.grid}>
-        <GridLayout />
+      <div>
+        <GridLayout items={items} />
       </div>
     );
   };
 
-  export { Footer };
+export { Footer };
--- src/components/footer/GridLayout.js
+++ src/components/footer/GridLayout.js
@@ -1,22 +1,20 @@
 import React from "react";
-import "./GridLayout.css"; // Create this CSS file to style the grid layout.
+import "./GridLayout.css";
 
-const GridLayout = () => {
-  return (
-    <div className="grid-container">
-      {/* Content for the first row */}
-      <div className="grid-item">Item 1</div>
-      <div className="grid-item">Item 2</div>
-      <div className="grid-item">Item 3</div>
-      <div className="grid-item">Item 4</div>
-
-      {/* Content for the second row */}
-      <div className="grid-item">Item 5</div>
-      <div className="grid-item">Item 6</div>
-      <div className="grid-item">Item 7</div>
-      <div className="grid-item">Item 8</div>
-    </div>
-  );
-};
+const GridLayout = ({ items }) => {
+    return (
+      <div className="grid-container">
+        {items.map((item, index) => (
+          <div key={index} className="grid-item">
+            <img src={item.photo} alt=""/>
+            <div className="item-text">
+              <p className="section1">{item.section1}</p>
+              <p className="section2">{item.section2}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
 export default GridLayout;
