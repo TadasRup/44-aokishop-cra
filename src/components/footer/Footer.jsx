069d2d97f50c1d1f79ec277378a32fbdd2825257
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-20 09:33:09 +0300

    Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import style from './Footer.module.css';
+
+export function Footer() {
+    return (
+        <footer className={style.footer}>
+            <div className={style.footertext}>
+                <h4>Operations Asistant</h4>
+            </div>
+        </footer>
+    )
+}
